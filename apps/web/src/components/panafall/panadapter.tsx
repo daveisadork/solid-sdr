3c2ac014f34e172455439fe1c08181119ba408c1
Dave Hayes <dwhayes@gmail.com>
2026-04-16 11:59:01 -0500

    Extract spots to a separate file

--- apps/web/src/components/panafall/panadapter.tsx
+++ apps/web/src/components/panafall/panadapter.tsx
@@ -29,81 +29,7 @@ import {
   TooltipContent,
   TooltipTrigger,
 } from "../ui/tooltip";
-
-const FONT_SIZES = [
-  "text-xs",
-  "text-sm",
-  "text-md",
-  "text-lg",
-  "text-xl",
-  "text-2xl",
-  "text-3xl",
-  "text-4xl",
-  "text-5xl",
-  "text-6xl",
-  "text-7xl",
-  "text-8xl",
-  "text-9xl",
-];
-
-function Spots(props: { pan: PanadapterState }) {
-  const { state, radio } = useFlexRadio();
-  const { freqToX, spotIds } = usePanafall();
-  const { preferences } = usePreferences();
-
-  return (
-    <div
-      class="absolute inset-1 translate-x-(--drag-offset) -translate-y-(--spots-position) z-20 pointer-events-none"
-      classList={{
-        "bottom-4": preferences.enableTransparencyEffects,
-        [FONT_SIZES[preferences.spots.fontSize]]: true,
-      }}
-      style={{
-        "--spots-position": `${preferences.spots.position}%`,
-        "--spot-spacing": `${preferences.spots.verticalSpacing}%`,
-      }}
-    >
-      <For each={spotIds()}>
-        {(spotId, index) => (
-          <Show when={state.status.spot[spotId]}>
-            {(getSpot) => {
-              const spot = getSpot();
-              const offset = () => index() % preferences.spots.levels;
-              return (
-                <Tooltip gutter={0}>
-                  <TooltipTrigger
-                    class="font-mono absolute px-1 bottom-0 left-(--spot-x-offset) -translate-y-(--spot-lane) border rounded-sm text-(--spot-color) border-(--spot-color) bg-(--spot-background-color)/80 z-(--spot-z-index) cursor-pointer pointer-events-auto shadow-sm shadow-black"
-                    style={{
-                      "--spot-z-index": 0 - spot.priority,
-                      "--spot-x-offset": `${freqToX(spot.rxFreqMHz)}px`,
-                      "--spot-color": spot.color,
-                      "--spot-background-color":
-                        spot.backgroundColor ?? "var(--background)",
-                      "--spot-lane": `calc(var(--spot-spacing) + ${offset() * (100 + preferences.spots.verticalSpacing)}%)`,
-                      // "--spot-y-offset": `calc(var(--spacing) * ${(index() % preferences.spots.levels) * 5})`,
-                    }}
-                    onClick={() => {
-                      radio()?.spot(spotId)?.trigger(props.pan.streamId);
-                    }}
-                  >
-                    {spot.callsign}
-                  </TooltipTrigger>
-                  <TooltipContent class="overflow-visible">
-                    <TooltipArrow />
-                    <div>
-                      {new Date(spot.timestampSec * 1000).toLocaleString()}
-                    </div>
-                    <div>{spot.comment}</div>
-                  </TooltipContent>
-                </Tooltip>
-              );
-            }}
-          </Show>
-        )}
-      </For>
-    </div>
-  );
-}
+import { Spots } from "./spots";
 
 export function Panadapter(props: {
   pan: PanadapterState;
@@ -138,7 +64,7 @@ export function Panadapter(props: {
     });
   });
 
-  const { slices, setPanadapterControlsRef, spotIds, freqToX } = usePanafall();
+  const { slices, setPanadapterControlsRef } = usePanafall();
 
   createEffect(() => {
     const canvas = canvasRef();

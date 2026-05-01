becb3d798e82c2ebd11e3b02a66da3a27bb93633
Dave Hayes <dwhayes@gmail.com>
2026-05-01 07:21:34 -0500

    Fix up preferences and default merging

--- apps/web/src/context/preferences.tsx
+++ apps/web/src/context/preferences.tsx
@@ -5,8 +5,9 @@ import {
   type ParentComponent,
   useContext,
   createEffect,
+  onMount,
 } from "solid-js";
-import { createStore, SetStoreFunction } from "solid-js/store";
+import { createStore, reconcile, SetStoreFunction } from "solid-js/store";
 import { DaxChannelMode } from "~/lib/dax-audio-sink/types";
 import { ControlTarget, SliceSelector } from "./controls";
 
@@ -36,17 +37,17 @@ export interface SpotPreferences {
   position: number;
   verticalSpacing: number;
   fontSize: number;
-  overrideColor: string | null;
-  overrideBackgroundColor: string | null;
+  overrideColor?: string | null;
+  overrideBackgroundColor?: string | null;
 }
 
 export type MidiSource =
-  | { port: string | null; channel: number; kind: "cc" | "note"; id: number }
+  | { port?: string | null; channel: number; kind: "cc" | "note"; id: number }
   | {
-      port: string | null;
+      port?: string | null;
       channel: number;
       kind: "pitchBend";
-      id: null;
+      id?: null;
     };
 
 type MidiControlRef = {
@@ -189,264 +190,273 @@ const defaultDaxConfig = () => {
   return config;
 };
 
-const initialPreferences = () =>
-  ({
-    stationName: "",
-    smoothScroll: true,
-    enableBlurEffects: true,
-    enableTransparencyEffects: true,
-    showDisplayMarkers: true,
-    midiMappings: [],
-    spots: {
-      enabled: true,
-      levels: 3,
-      position: 0,
-      fontSize: 1,
-      verticalSpacing: 20,
-      overrideColor: null,
-      overrideBackgroundColor: null,
-    },
-    peakStyle: "points",
-    fillStyle: "solid",
-    gradientStyle: "color",
-    meterStyle: "smooth",
-    theme: "dark",
-    panBackgroundColor: "#02517e",
-    showFps: false,
-    sMeterEnabled: true,
-    showTuningGuide: false,
-    preventScreenSleep: false,
-    panadapterSettingsStyle: "floating",
-    panadapterSizes: [],
-    enableRemoteAudio: true,
+const getDefaults = (): Preferences => ({
+  stationName: "",
+  smoothScroll: true,
+  enableBlurEffects: true,
+  enableTransparencyEffects: true,
+  showDisplayMarkers: true,
+  midiMappings: [],
+  spots: {
+    enabled: true,
+    levels: 3,
+    position: 0,
+    fontSize: 1,
+    verticalSpacing: 20,
+    overrideColor: null,
+    overrideBackgroundColor: null,
+  },
+  peakStyle: "points",
+  fillStyle: "solid",
+  gradientStyle: "color",
+  meterStyle: "smooth",
+  theme: "dark",
+  panBackgroundColor: "#02517e",
+  showFps: false,
+  sMeterEnabled: true,
+  showTuningGuide: false,
+  preventScreenSleep: false,
+  panadapterSettingsStyle: "floating",
+  panadapterSizes: [],
+  enableRemoteAudio: true,
+  inputDeviceId: "default",
+  outputDeviceId: "default",
+  radioPanelOpen: true,
+  sidebarPanels: ["tx", "p-cw", "phone", "rx", "eq"],
+  panadapterSettingsOpen: [false, false, false, false],
+  daxRxConfig: defaultDaxConfig(),
+  daxTxConfig: {
+    enabled: false,
     inputDeviceId: "default",
-    outputDeviceId: "default",
-    radioPanelOpen: true,
-    sidebarPanels: ["tx", "p-cw", "phone", "rx", "eq"],
-    panadapterSettingsOpen: [false, false, false, false],
-    daxRxConfig: defaultDaxConfig(),
-    daxTxConfig: {
-      enabled: false,
-      inputDeviceId: "default",
-      reducedBandwidth: true,
-      channelMode: "both",
-    },
-    palette: {
-      gradients: [
-        {
-          name: "SmartSDR",
-          stops: [
-            { color: "#000000", offset: 0.0 },
-            { color: "#0000ff", offset: 0.15 },
-            { color: "#00ffff", offset: 0.25 },
-            { color: "#00ff00", offset: 0.35 },
-            { color: "#ffff00", offset: 0.55 },
-            { color: "#ff0000", offset: 0.9 },
-            { color: "#ffffff", offset: 1.0 },
-          ],
-        },
-        {
-          name: "SmartSDR + Purple",
-          stops: [
-            { color: "#000000", offset: 0.0 },
-            { color: "#0000ff", offset: 0.15 },
-            { color: "#00ffff", offset: 0.225 },
-            { color: "#00ff00", offset: 0.3 },
-            { color: "#ffff00", offset: 0.45 },
-            { color: "#ff0000", offset: 0.6 },
-            { color: "#ff00ff", offset: 0.75 },
-            { color: "#ffffff", offset: 1.0 },
-          ],
-        },
+    reducedBandwidth: true,
+    channelMode: "both",
+  },
+  palette: {
+    gradients: [
+      {
+        name: "SmartSDR",
+        stops: [
+          { color: "#000000", offset: 0.0 },
+          { color: "#0000ff", offset: 0.15 },
+          { color: "#00ffff", offset: 0.25 },
+          { color: "#00ff00", offset: 0.35 },
+          { color: "#ffff00", offset: 0.55 },
+          { color: "#ff0000", offset: 0.9 },
+          { color: "#ffffff", offset: 1.0 },
+        ],
+      },
+      {
+        name: "SmartSDR + Purple",
+        stops: [
+          { color: "#000000", offset: 0.0 },
+          { color: "#0000ff", offset: 0.15 },
+          { color: "#00ffff", offset: 0.225 },
+          { color: "#00ff00", offset: 0.3 },
+          { color: "#ffff00", offset: 0.45 },
+          { color: "#ff0000", offset: 0.6 },
+          { color: "#ff00ff", offset: 0.75 },
+          { color: "#ffffff", offset: 1.0 },
+        ],
+      },
 
-        {
-          name: "Dark",
-          stops: [
-            { color: "#000000", offset: 0.0 },
-            { color: "#0000ff", offset: 0.65 },
-            { color: "#00ff00", offset: 0.9 },
-            { color: "#ff0000", offset: 0.95 },
-            { color: "#ffb6c1", offset: 1.0 },
-          ],
-        },
-        {
-          name: "Grayscale",
-          stops: [
-            { color: "#000000", offset: 0.0 },
-            { color: "#ffffff", offset: 1.0 },
-          ],
-        },
-        {
-          name: "Deuteranopia",
-          stops: [
-            { color: "#000000", offset: 0.0 },
-            { color: "#083c6b", offset: 0.15 },
-            { color: "#84a2d6", offset: 0.5 },
-            { color: "#a59673", offset: 0.65 },
-            { color: "#ffff00", offset: 0.75 },
-            { color: "#ffff00", offset: 0.95 },
-            { color: "#ffffff", offset: 1.0 },
-          ],
-        },
-        {
-          name: "Tritanopia",
-          stops: [
-            { color: "#000000", offset: 0.0 },
-            { color: "#004552", offset: 0.15 },
-            { color: "#6bbad6", offset: 0.45 },
-            { color: "#4a0818", offset: 0.46 },
-            { color: "#ff0000", offset: 0.9 },
-            { color: "#d67984", offset: 0.99 },
-            { color: "#ffffff", offset: 1.0 },
-          ],
-        },
-        // {
-        //   name: "Vintage Warm",
-        //   stops: [
-        //     { color: "#000000", offset: 0.0 },
-        //     { color: "#1d4877", offset: 0.15 },
-        //     { color: "#1ba4a1", offset: 0.25 },
-        //     { color: "#1b8a5a", offset: 0.35 },
-        //     { color: "#fbb021", offset: 0.55 },
-        //     { color: "#ee3e32", offset: 0.6 },
-        //     { color: "#ffffff", offset: 1.0 },
-        //   ],
-        // },
-        // {
-        //   name: "Vintage Warm + Pink",
-        //   stops: [
-        //     { color: "#000000", offset: 0.0 },
-        //     { color: "#1d4877", offset: 0.15 },
-        //     { color: "#1ba4a1", offset: 0.225 },
-        //     { color: "#1b8a5a", offset: 0.3 },
-        //     { color: "#fbb021", offset: 0.45 },
-        //     { color: "#f68838", offset: 0.525 },
-        //     { color: "#ee3e32", offset: 0.6 },
-        //     { color: "#f36a82", offset: 0.75 },
-        //     { color: "#ffffff", offset: 1.0 },
-        //   ],
-        // },
-        // {
-        //   name: "CMYK",
-        //   stops: [
-        //     { color: "#000000", offset: 0.0 },
-        //     { color: "#00ffff", offset: 0.25 },
-        //     { color: "#ffff00", offset: 0.5 },
-        //     { color: "#ff00ff", offset: 0.75 },
-        //     { color: "#ffffff", offset: 1.0 },
-        //   ],
-        // },
-        // {
-        //   name: "RGB",
-        //   stops: [
-        //     { color: "#000000", offset: 0.0 },
-        //     { color: "#0000ff", offset: 0.25 },
-        //     { color: "#00ff00", offset: 0.5 },
-        //     { color: "#ff0000", offset: 0.75 },
-        //     { color: "#ffffff", offset: 1.0 },
-        //   ],
-        // },
-        // {
-        //   name: "Solarized",
-        //   stops: [
-        //     { color: "#002b36", offset: 0.0 },
-        //     { color: "#268bd2", offset: 0.15 },
-        //     { color: "#2aa198", offset: 0.25 },
-        //     { color: "#859900", offset: 0.35 },
-        //     { color: "#b58900", offset: 0.55 },
-        //     { color: "#dc322f", offset: 0.9 },
-        //     { color: "#ffffff", offset: 1.0 },
-        //   ],
-        // },
-        // {
-        //   name: "Solarized + Pink",
-        //   stops: [
-        //     { color: "#002b36", offset: 0.0 },
-        //     { color: "#268bd2", offset: 0.15 },
-        //     { color: "#2aa198", offset: 0.225 },
-        //     { color: "#859900", offset: 0.3 },
-        //     { color: "#b58900", offset: 0.45 },
-        //     { color: "#cb4b16", offset: 0.525 },
-        //     { color: "#dc322f", offset: 0.6 },
-        //     { color: "#d33682", offset: 0.75 },
-        //     { color: "#ffffff", offset: 1.0 },
-        //   ],
-        // },
-        // {
-        //   name: "High Contrast",
-        //   stops: [
-        //     { color: "#000000", offset: 0.0 },
-        //     { color: "#000080", offset: 0.15 },
-        //     { color: "#00ffff", offset: 0.25 },
-        //     { color: "#00ff00", offset: 0.3 },
-        //     { color: "#ffff00", offset: 0.45 },
-        //     { color: "#ff0000", offset: 0.6 },
-        //     { color: "#ff00ff", offset: 0.75 },
-        //     { color: "#ffffff", offset: 1.0 },
-        //   ],
-        // },
-        // {
-        //   name: "Contrasty",
-        //   stops: [
-        //     { color: "#000000", offset: 0.1 },
-        //     // { color: "#400040", offset: 0.2 },
-        //     { color: "#2a1049", offset: 0.2 },
-        //     // { color: "#1f6a96", offset: 0.3 },
-        //     { color: "#0090e2", offset: 0.4 },
-        //     { color: "#009bb1", offset: 0.5 },
-        //     { color: "#bedf0d", offset: 0.6 },
-        //     // { color: "#ff0000", offset: 0.6 },
-        //     { color: "#ff00ff", offset: 0.7 },
-        //     { color: "#ffffff", offset: 1.0 },
-        //   ],
-        // },
-        {
-          name: "Spectral",
-          stops: [
-            { color: "#000000", offset: 0.1 },
-            { color: "#330033", offset: 0.2 },
-            { color: "#00ffff", offset: 0.3 },
-            { color: "#ffff00", offset: 0.5 },
-            { color: "#ff00ff", offset: 0.7 },
-            { color: "#ffffff", offset: 1.0 },
-          ],
-        },
-        {
-          name: "Aurora",
-          stops: [
-            { color: "#000000", offset: 0.1 },
-            { color: "#5f2a84", offset: 0.2 },
-            { color: "#524096", offset: 0.3 },
-            { color: "#2082a6", offset: 0.4 },
-            { color: "#01cbae", offset: 0.5 },
-            { color: "#01efac", offset: 0.6 },
-            // { color: "#ff0000", offset: 0.6 },
-            // { color: "#ff00ff", offset: 0.7 },
-            { color: "#ffffff", offset: 1.0 },
-          ],
-        },
-      ],
-    },
-  }) as Preferences;
+      {
+        name: "Dark",
+        stops: [
+          { color: "#000000", offset: 0.0 },
+          { color: "#0000ff", offset: 0.65 },
+          { color: "#00ff00", offset: 0.9 },
+          { color: "#ff0000", offset: 0.95 },
+          { color: "#ffb6c1", offset: 1.0 },
+        ],
+      },
+      {
+        name: "Grayscale",
+        stops: [
+          { color: "#000000", offset: 0.0 },
+          { color: "#ffffff", offset: 1.0 },
+        ],
+      },
+      {
+        name: "Deuteranopia",
+        stops: [
+          { color: "#000000", offset: 0.0 },
+          { color: "#083c6b", offset: 0.15 },
+          { color: "#84a2d6", offset: 0.5 },
+          { color: "#a59673", offset: 0.65 },
+          { color: "#ffff00", offset: 0.75 },
+          { color: "#ffff00", offset: 0.95 },
+          { color: "#ffffff", offset: 1.0 },
+        ],
+      },
+      {
+        name: "Tritanopia",
+        stops: [
+          { color: "#000000", offset: 0.0 },
+          { color: "#004552", offset: 0.15 },
+          { color: "#6bbad6", offset: 0.45 },
+          { color: "#4a0818", offset: 0.46 },
+          { color: "#ff0000", offset: 0.9 },
+          { color: "#d67984", offset: 0.99 },
+          { color: "#ffffff", offset: 1.0 },
+        ],
+      },
+      // {
+      //   name: "Vintage Warm",
+      //   stops: [
+      //     { color: "#000000", offset: 0.0 },
+      //     { color: "#1d4877", offset: 0.15 },
+      //     { color: "#1ba4a1", offset: 0.25 },
+      //     { color: "#1b8a5a", offset: 0.35 },
+      //     { color: "#fbb021", offset: 0.55 },
+      //     { color: "#ee3e32", offset: 0.6 },
+      //     { color: "#ffffff", offset: 1.0 },
+      //   ],
+      // },
+      // {
+      //   name: "Vintage Warm + Pink",
+      //   stops: [
+      //     { color: "#000000", offset: 0.0 },
+      //     { color: "#1d4877", offset: 0.15 },
+      //     { color: "#1ba4a1", offset: 0.225 },
+      //     { color: "#1b8a5a", offset: 0.3 },
+      //     { color: "#fbb021", offset: 0.45 },
+      //     { color: "#f68838", offset: 0.525 },
+      //     { color: "#ee3e32", offset: 0.6 },
+      //     { color: "#f36a82", offset: 0.75 },
+      //     { color: "#ffffff", offset: 1.0 },
+      //   ],
+      // },
+      // {
+      //   name: "CMYK",
+      //   stops: [
+      //     { color: "#000000", offset: 0.0 },
+      //     { color: "#00ffff", offset: 0.25 },
+      //     { color: "#ffff00", offset: 0.5 },
+      //     { color: "#ff00ff", offset: 0.75 },
+      //     { color: "#ffffff", offset: 1.0 },
+      //   ],
+      // },
+      // {
+      //   name: "RGB",
+      //   stops: [
+      //     { color: "#000000", offset: 0.0 },
+      //     { color: "#0000ff", offset: 0.25 },
+      //     { color: "#00ff00", offset: 0.5 },
+      //     { color: "#ff0000", offset: 0.75 },
+      //     { color: "#ffffff", offset: 1.0 },
+      //   ],
+      // },
+      // {
+      //   name: "Solarized",
+      //   stops: [
+      //     { color: "#002b36", offset: 0.0 },
+      //     { color: "#268bd2", offset: 0.15 },
+      //     { color: "#2aa198", offset: 0.25 },
+      //     { color: "#859900", offset: 0.35 },
+      //     { color: "#b58900", offset: 0.55 },
+      //     { color: "#dc322f", offset: 0.9 },
+      //     { color: "#ffffff", offset: 1.0 },
+      //   ],
+      // },
+      // {
+      //   name: "Solarized + Pink",
+      //   stops: [
+      //     { color: "#002b36", offset: 0.0 },
+      //     { color: "#268bd2", offset: 0.15 },
+      //     { color: "#2aa198", offset: 0.225 },
+      //     { color: "#859900", offset: 0.3 },
+      //     { color: "#b58900", offset: 0.45 },
+      //     { color: "#cb4b16", offset: 0.525 },
+      //     { color: "#dc322f", offset: 0.6 },
+      //     { color: "#d33682", offset: 0.75 },
+      //     { color: "#ffffff", offset: 1.0 },
+      //   ],
+      // },
+      // {
+      //   name: "High Contrast",
+      //   stops: [
+      //     { color: "#000000", offset: 0.0 },
+      //     { color: "#000080", offset: 0.15 },
+      //     { color: "#00ffff", offset: 0.25 },
+      //     { color: "#00ff00", offset: 0.3 },
+      //     { color: "#ffff00", offset: 0.45 },
+      //     { color: "#ff0000", offset: 0.6 },
+      //     { color: "#ff00ff", offset: 0.75 },
+      //     { color: "#ffffff", offset: 1.0 },
+      //   ],
+      // },
+      // {
+      //   name: "Contrasty",
+      //   stops: [
+      //     { color: "#000000", offset: 0.1 },
+      //     // { color: "#400040", offset: 0.2 },
+      //     { color: "#2a1049", offset: 0.2 },
+      //     // { color: "#1f6a96", offset: 0.3 },
+      //     { color: "#0090e2", offset: 0.4 },
+      //     { color: "#009bb1", offset: 0.5 },
+      //     { color: "#bedf0d", offset: 0.6 },
+      //     // { color: "#ff0000", offset: 0.6 },
+      //     { color: "#ff00ff", offset: 0.7 },
+      //     { color: "#ffffff", offset: 1.0 },
+      //   ],
+      // },
+      {
+        name: "Spectral",
+        stops: [
+          { color: "#000000", offset: 0.1 },
+          { color: "#330033", offset: 0.2 },
+          { color: "#00ffff", offset: 0.3 },
+          { color: "#ffff00", offset: 0.5 },
+          { color: "#ff00ff", offset: 0.7 },
+          { color: "#ffffff", offset: 1.0 },
+        ],
+      },
+      {
+        name: "Aurora",
+        stops: [
+          { color: "#000000", offset: 0.1 },
+          { color: "#5f2a84", offset: 0.2 },
+          { color: "#524096", offset: 0.3 },
+          { color: "#2082a6", offset: 0.4 },
+          { color: "#01cbae", offset: 0.5 },
+          { color: "#01efac", offset: 0.6 },
+          // { color: "#ff0000", offset: 0.6 },
+          // { color: "#ff00ff", offset: 0.7 },
+          { color: "#ffffff", offset: 1.0 },
+        ],
+      },
+    ],
+  },
+});
+
+const deepMerge = <T extends object>(target: T, source: T): T => {
+  for (const [key, value] of Object.entries(source)) {
+    const targetValue = target[key];
+    if (targetValue === value) continue;
+    if (targetValue === undefined) {
+      if (Array.isArray(target)) target[key] = value;
+      continue;
+    }
+    target[key] =
+      typeof value === "object" && value !== null
+        ? deepMerge(target[key], value)
+        : value;
+  }
+  return target;
+};
 
 export const PreferencesProvider: ParentComponent = (props) => {
-  // const initial = initialPreferences();
-  const [store, setStore] = createStore(initialPreferences());
+  const [store, setStore] = createStore(getDefaults());
   const [preferences, setPreferences] = makePersisted([store, setStore], {
     name: "preferences",
   });
 
-  // override any missing keys in the loaded preferences with defaults from initialPreferences
-  setPreferences({ ...initialPreferences(), ...preferences });
-
-  // onMount(() => {
-  //   Object.keys(preferences)
-  //     .filter((key) => !(key in initial))
-  //     .forEach((key) => {
-  //       console.log(`Removing old preference key: ${key}`);
-  //       setPreferences(key, undefined);
-  //     });
-  // });
+  // populate any missing defaults, and clean up any deprecated/removed prefs
+  setPreferences(
+    reconcile(
+      deepMerge(getDefaults(), JSON.parse(JSON.stringify(preferences))),
+    ),
+  );
 
   createEffect(() => {
     if (!preferences.enableTransparencyEffects) {

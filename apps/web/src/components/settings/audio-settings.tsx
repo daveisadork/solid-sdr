27f4ff2bf93fc1a3e75843f712db3072272e3c0f
Dave Hayes <dwhayes@gmail.com>
2026-05-08 19:50:41 -0500

    Fix radio audio settings

--- apps/web/src/components/settings/audio-settings.tsx
+++ apps/web/src/components/settings/audio-settings.tsx
@@ -44,7 +44,7 @@ import { ToggleGroupItem } from "../ui/toggle-group";
 function InnerAudioSettings() {
   const { audioStreams } = useRuntime();
   const { preferences, setPreferences } = usePreferences();
-  const { radio, state } = useFlexRadio();
+  const { state } = useFlexRadio();
   const [caps, setCaps] = createSignal<string[]>([]);
 
   const outputs = createSpeakers();
@@ -85,210 +85,138 @@ function InnerAudioSettings() {
   });
 
   return (
-    <div
-      class="relative flex flex-col gap-4 text-sm overflow-y-auto shrink"
-      style={{ "scrollbar-width": "thin" }}
-    >
-      <Card class="bg-transparent">
-        <CardHeader>
-          <CardTitle>Radio Audio</CardTitle>
-        </CardHeader>
-        <CardContent class="flex flex-col gap-4">
-          <div class="flex gap-1 items-center">
-            <ToggleButton
-              class="aspect-square size-10 inline-flex items-center p-1 rounded-md transition-colors hover:bg-accent"
-              pressed={state.status.radio.lineoutMute}
-              onChange={(pressed) => radio().setLineoutMute(pressed)}
-            >
-              <Dynamic
-                component={
-                  state.status.radio.lineoutMute ? MdiSpeakerOff : MdiSpeaker
-                }
-                class="size-full"
-              />
-            </ToggleButton>
-            <SimpleSlider
-              value={[state.status.radio.lineoutGain]}
-              onChange={([value]) => radio().setLineoutGain(value)}
-              minValue={0}
-              maxValue={100}
-              label="Line-Out Level"
-              getValueLabel={({ values: [value] }) => `${value}%`}
-            />
-          </div>
-          <div class="flex gap-1 items-center">
-            <ToggleButton
-              class="aspect-square size-10 inline-flex items-center p-1 rounded-md transition-colors hover:bg-accent"
-              pressed={state.status.radio.headphoneMute}
-              onChange={(pressed) => radio().setHeadphoneMute(pressed)}
-            >
-              <Dynamic
-                component={
-                  state.status.radio.headphoneMute
-                    ? MdiHeadphonesOff
-                    : MdiHeadphones
-                }
-                class="size-full"
-              />
-            </ToggleButton>
-            <SimpleSlider
-              value={[state.status.radio.headphoneGain]}
-              onChange={([value]) => radio().setHeadphoneGain(value)}
-              minValue={0}
-              maxValue={100}
-              label="Headphone Level"
-              getValueLabel={({ values: [value] }) => `${value}%`}
-            />
-          </div>
-        </CardContent>
-      </Card>
-      <Card class="bg-transparent">
-        <CardHeader>
-          <SwitchRoot
-            class="flex"
-            checked={preferences.remoteAudio.rx.enabled}
-            onChange={(checked) =>
-              setPreferences("remoteAudio", "rx", "enabled", checked)
-            }
-          >
-            <SwitchLabel class="grow flex items-center">
-              <CardTitle>Remote Audio</CardTitle>
-            </SwitchLabel>
-            <SwitchControl>
-              <SwitchThumb />
-            </SwitchControl>
-          </SwitchRoot>
-        </CardHeader>
-        <CardContent class="flex flex-col gap-4">
-          <SimpleSwitch
-            checked={!preferences.remoteAudio.tx.enabled}
-            onChange={(checked) =>
-              setPreferences("remoteAudio", "tx", "enabled", !checked)
-            }
-            label="RX Only"
-          />
-          <Select
-            class="flex flex-col gap-2 grow shrink overflow-hidden"
-            value={preferences.remoteAudio.rx.outputDeviceId}
-            onChange={(value: string) => {
-              if (!value) return;
-              setPreferences("remoteAudio", "rx", "outputDeviceId", value);
-            }}
-            options={outputs().map((d) => d.deviceId)}
-            itemComponent={(props) => {
-              return (
-                <SelectItem item={props.item}>
-                  {
-                    outputs().find((d) => d.deviceId === props.item.rawValue)
-                      ?.label
-                  }
-                </SelectItem>
-              );
-            }}
-          >
-            <SelectLabel class="flex items-center gap-1">
-              <MaterialSymbolsSpeaker />
-              Output Device
-            </SelectLabel>
-            <SelectTrigger>
-              <SelectValue class="overflow-hidden text-ellipsis whitespace-nowrap">
-                {(state) =>
-                  outputs().find((d) => d.deviceId === state.selectedOption())
-                    ?.label || "Select Audio Output"
-                }
-              </SelectValue>
-            </SelectTrigger>
-            <SelectContent />
-          </Select>
-          <Select
-            class="flex flex-col gap-2 grow shrink overflow-hidden"
-            value={preferences.remoteAudio.tx.inputDeviceId}
-            onChange={(value: string) => {
-              if (!value) return;
-              setPreferences("remoteAudio", "tx", "inputDeviceId", value);
-            }}
-            options={inputs().map((d) => d.deviceId)}
-            itemComponent={(props) => (
+    <Card class="bg-transparent">
+      <CardHeader>
+        <SwitchRoot
+          class="flex"
+          checked={preferences.remoteAudio.rx.enabled}
+          onChange={(checked) =>
+            setPreferences("remoteAudio", "rx", "enabled", checked)
+          }
+        >
+          <SwitchLabel class="grow flex items-center">
+            <CardTitle>Remote Audio</CardTitle>
+          </SwitchLabel>
+          <SwitchControl>
+            <SwitchThumb />
+          </SwitchControl>
+        </SwitchRoot>
+      </CardHeader>
+      <CardContent class="flex flex-col gap-4">
+        <SimpleSwitch
+          checked={!preferences.remoteAudio.tx.enabled}
+          onChange={(checked) =>
+            setPreferences("remoteAudio", "tx", "enabled", !checked)
+          }
+          label="RX Only"
+        />
+        <Select
+          class="flex flex-col gap-2 grow shrink overflow-hidden"
+          value={preferences.remoteAudio.rx.outputDeviceId}
+          onChange={(value: string) => {
+            if (!value) return;
+            setPreferences("remoteAudio", "rx", "outputDeviceId", value);
+          }}
+          options={outputs().map((d) => d.deviceId)}
+          itemComponent={(props) => {
+            return (
               <SelectItem item={props.item}>
-                {inputs().find((d) => d.deviceId === props.item.rawValue)
-                  ?.label ?? "Default"}
+                {
+                  outputs().find((d) => d.deviceId === props.item.rawValue)
+                    ?.label
+                }
               </SelectItem>
-            )}
-          >
-            <SelectLabel class="flex items-center gap-1">
-              <MaterialSymbolsMic />
-              Input Device
-            </SelectLabel>
-            <SelectTrigger>
-              <SelectValue class="overflow-hidden text-ellipsis whitespace-nowrap">
-                {(state) =>
-                  inputs().find((d) => d.deviceId === state.selectedOption())
-                    ?.label || "Select Audio Input"
-                }
-              </SelectValue>
-            </SelectTrigger>
-            <SelectContent />
-          </Select>
-          <Show when={caps().length} fallback="Checking capabilities...">
-            <Show when={caps().includes("autoGainControl")}>
-              <SimpleSwitch
-                checked={preferences.remoteAudio.tx.autoGainControl}
-                onChange={(checked) =>
-                  setPreferences(
-                    "remoteAudio",
-                    "tx",
-                    "autoGainControl",
-                    checked,
-                  )
-                }
-                label="Auto Gain Control"
-              />
-            </Show>
-            <Show when={caps().includes("echoCancellation")}>
-              <SimpleSwitch
-                checked={preferences.remoteAudio.tx.echoCancellation}
-                onChange={(checked) =>
-                  setPreferences(
-                    "remoteAudio",
-                    "tx",
-                    "echoCancellation",
-                    checked,
-                  )
-                }
-                label="Echo Cancellation"
-              />
-            </Show>
-            <Show when={caps().includes("noiseSuppression")}>
-              <SimpleSwitch
-                checked={preferences.remoteAudio.tx.noiseSuppression}
-                onChange={(checked) =>
-                  setPreferences(
-                    "remoteAudio",
-                    "tx",
-                    "noiseSuppression",
-                    checked,
-                  )
-                }
-                label="Noise Suppression"
-              />
-            </Show>
-            <Show when={caps().includes("voiceIsolation")}>
-              <SimpleSwitch
-                checked={preferences.remoteAudio.tx.voiceIsolation}
-                onChange={(checked) =>
-                  setPreferences("remoteAudio", "tx", "voiceIsolation", checked)
-                }
-                label="Voice Isolation"
-              />
-            </Show>
+            );
+          }}
+        >
+          <SelectLabel class="flex items-center gap-1">
+            <MaterialSymbolsSpeaker />
+            Output Device
+          </SelectLabel>
+          <SelectTrigger>
+            <SelectValue class="overflow-hidden text-ellipsis whitespace-nowrap">
+              {(state) =>
+                outputs().find((d) => d.deviceId === state.selectedOption())
+                  ?.label || "Select Audio Output"
+              }
+            </SelectValue>
+          </SelectTrigger>
+          <SelectContent />
+        </Select>
+        <Select
+          class="flex flex-col gap-2 grow shrink overflow-hidden"
+          value={preferences.remoteAudio.tx.inputDeviceId}
+          onChange={(value: string) => {
+            if (!value) return;
+            setPreferences("remoteAudio", "tx", "inputDeviceId", value);
+          }}
+          options={inputs().map((d) => d.deviceId)}
+          itemComponent={(props) => (
+            <SelectItem item={props.item}>
+              {inputs().find((d) => d.deviceId === props.item.rawValue)
+                ?.label ?? "Default"}
+            </SelectItem>
+          )}
+        >
+          <SelectLabel class="flex items-center gap-1">
+            <MaterialSymbolsMic />
+            Input Device
+          </SelectLabel>
+          <SelectTrigger>
+            <SelectValue class="overflow-hidden text-ellipsis whitespace-nowrap">
+              {(state) =>
+                inputs().find((d) => d.deviceId === state.selectedOption())
+                  ?.label || "Select Audio Input"
+              }
+            </SelectValue>
+          </SelectTrigger>
+          <SelectContent />
+        </Select>
+        <Show when={caps().length} fallback="Checking capabilities...">
+          <Show when={caps().includes("autoGainControl")}>
+            <SimpleSwitch
+              checked={preferences.remoteAudio.tx.autoGainControl}
+              onChange={(checked) =>
+                setPreferences("remoteAudio", "tx", "autoGainControl", checked)
+              }
+              label="Auto Gain Control"
+            />
           </Show>
-        </CardContent>
-      </Card>
-    </div>
+          <Show when={caps().includes("echoCancellation")}>
+            <SimpleSwitch
+              checked={preferences.remoteAudio.tx.echoCancellation}
+              onChange={(checked) =>
+                setPreferences("remoteAudio", "tx", "echoCancellation", checked)
+              }
+              label="Echo Cancellation"
+            />
+          </Show>
+          <Show when={caps().includes("noiseSuppression")}>
+            <SimpleSwitch
+              checked={preferences.remoteAudio.tx.noiseSuppression}
+              onChange={(checked) =>
+                setPreferences("remoteAudio", "tx", "noiseSuppression", checked)
+              }
+              label="Noise Suppression"
+            />
+          </Show>
+          <Show when={caps().includes("voiceIsolation")}>
+            <SimpleSwitch
+              checked={preferences.remoteAudio.tx.voiceIsolation}
+              onChange={(checked) =>
+                setPreferences("remoteAudio", "tx", "voiceIsolation", checked)
+              }
+              label="Voice Isolation"
+            />
+          </Show>
+        </Show>
+      </CardContent>
+    </Card>
   );
 }
 
 export function AudioSettings() {
+  const { radio, state } = useFlexRadio();
   const audioPermission = createPermission("microphone");
 
   createEffect(() => {
@@ -303,26 +231,88 @@ export function AudioSettings() {
       <DialogHeader>
         <DialogTitle>Audio Settings</DialogTitle>
       </DialogHeader>
-      <Switch
-        fallback={
-          <Callout>
-            <CalloutTitle>Audio Permissions Required</CalloutTitle>
-            <CalloutContent>Waiting for Audio Permissions...</CalloutContent>
-          </Callout>
-        }
+      <div
+        class="relative flex flex-col gap-4 text-sm overflow-y-auto shrink"
+        style={{ "scrollbar-width": "thin" }}
       >
-        <Match when={audioPermission() === "granted"}>
-          <InnerAudioSettings />
-        </Match>
-        <Match when={audioPermission() === "denied"}>
-          <Callout variant="error">
-            <CalloutTitle>Audio Permissions Denied</CalloutTitle>
-            <CalloutContent>
-              You must grant audio permissions to use audio features.
-            </CalloutContent>
-          </Callout>
-        </Match>
-      </Switch>
+        <Show when={radio()}>
+          <Card class="bg-transparent">
+            <CardHeader>
+              <CardTitle>Radio Audio</CardTitle>
+            </CardHeader>
+            <CardContent class="flex flex-col gap-4">
+              <div class="flex gap-1 items-center">
+                <ToggleButton
+                  class="aspect-square size-10 inline-flex items-center p-1 rounded-md transition-colors hover:bg-accent"
+                  pressed={state.status.radio.lineoutMute}
+                  onChange={(pressed) => radio().setLineoutMute(pressed)}
+                >
+                  <Dynamic
+                    component={
+                      state.status.radio.lineoutMute
+                        ? MdiSpeakerOff
+                        : MdiSpeaker
+                    }
+                    class="size-full"
+                  />
+                </ToggleButton>
+                <SimpleSlider
+                  value={[state.status.radio.lineoutGain]}
+                  onChange={([value]) => radio().setLineoutGain(value)}
+                  minValue={0}
+                  maxValue={100}
+                  label="Line-Out Level"
+                  getValueLabel={({ values: [value] }) => `${value}%`}
+                />
+              </div>
+              <div class="flex gap-1 items-center">
+                <ToggleButton
+                  class="aspect-square size-10 inline-flex items-center p-1 rounded-md transition-colors hover:bg-accent"
+                  pressed={state.status.radio.headphoneMute}
+                  onChange={(pressed) => radio().setHeadphoneMute(pressed)}
+                >
+                  <Dynamic
+                    component={
+                      state.status.radio.headphoneMute
+                        ? MdiHeadphonesOff
+                        : MdiHeadphones
+                    }
+                    class="size-full"
+                  />
+                </ToggleButton>
+                <SimpleSlider
+                  value={[state.status.radio.headphoneGain]}
+                  onChange={([value]) => radio().setHeadphoneGain(value)}
+                  minValue={0}
+                  maxValue={100}
+                  label="Headphone Level"
+                  getValueLabel={({ values: [value] }) => `${value}%`}
+                />
+              </div>
+            </CardContent>
+          </Card>
+        </Show>
+        <Switch
+          fallback={
+            <Callout>
+              <CalloutTitle>Audio Permissions Required</CalloutTitle>
+              <CalloutContent>Waiting for Audio Permissions...</CalloutContent>
+            </Callout>
+          }
+        >
+          <Match when={audioPermission() === "granted"}>
+            <InnerAudioSettings />
+          </Match>
+          <Match when={audioPermission() === "denied"}>
+            <Callout variant="error">
+              <CalloutTitle>Audio Permissions Denied</CalloutTitle>
+              <CalloutContent>
+                You must grant audio permissions to use audio features.
+              </CalloutContent>
+            </Callout>
+          </Match>
+        </Switch>
+      </div>
     </DialogContent>
   );
 }

de9dbfb9c72e45cbb5baec9702afe14e3bbaf32f
Dave Hayes <dwhayes@gmail.com>
2026-05-21 11:03:50 -0500

    Add a new release notification

---
+++ apps/web/src/components/release-notification.tsx
@@ -0,0 +1,72 @@
+import {
+  createEffect,
+  createResource,
+  createSignal,
+  onCleanup,
+  onMount,
+  Show,
+} from "solid-js";
+import { Callout, CalloutContent, CalloutTitle } from "./ui/callout";
+import { Button } from "./ui/button";
+import { APP_VERSION } from "~/lib/version";
+import createPresence from "solid-presence";
+import { Portal } from "solid-js/web";
+import MaterialSymbolsOpenInNew from "~icons/material-symbols/open-in-new";
+
+export function ReleaseNotification() {
+  const [hasNewVersion, setHasNewVersion] = createSignal(false);
+  const [dismissed, setDismissed] = createSignal(false);
+  const show = () => hasNewVersion() && !dismissed();
+  const [element, setElement] = createSignal<HTMLElement>(null);
+  const { present } = createPresence({
+    show,
+    element,
+  });
+  const [currentRelease, { refetch }] = createResource<{
+    tag_name: string;
+    name: string;
+    html_url: string;
+    draft: boolean;
+    prerelease: boolean;
+  }>(async () => {
+    const response = await fetch(
+      "https://api.github.com/repos/daveisadork/solid-sdr/releases/latest",
+    );
+    return await response.json();
+  });
+
+  createEffect(() => {
+    const currentVersion = currentRelease()?.tag_name;
+    if (!currentVersion) return;
+    setHasNewVersion(!APP_VERSION.startsWith(currentVersion));
+  });
+
+  onMount(() => {
+    const interval = setInterval(refetch, 300_000);
+    onCleanup(() => clearInterval(interval));
+  });
+
+  return (
+    <Show when={present()}>
+      <Portal>
+        <Callout
+          ref={setElement}
+          class="absolute fancy-bg-info! left-1/2 bottom-24 -translate-x-1/2 shadow-black/50 shadow-lg duration-2000 data-expanded:animate-in data-closed:animate-out data-closed:fade-out-0 data-expanded:fade-in-0 data-closed:slide-out-to-bottom data-expanded:slide-in-from-bottom data-closed:zoom-out-50 data-expanded:zoom-in-50"
+          data-closed={!show() ? "" : undefined}
+          data-expanded={show() ? "" : undefined}
+        >
+          <CalloutTitle class="text-foreground">
+            SolidSDR {currentRelease()?.name} is available!
+          </CalloutTitle>
+          <CalloutContent class="flex justify-end gap-2">
+            <Button onClick={() => setDismissed(true)}>Dismiss</Button>
+            <Button as="a" href={currentRelease()?.html_url} target="_blank">
+              <MaterialSymbolsOpenInNew />
+              Release Notes
+            </Button>
+          </CalloutContent>
+        </Callout>
+      </Portal>
+    </Show>
+  );
+}
